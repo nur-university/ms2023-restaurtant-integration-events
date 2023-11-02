022ad876fe68ef7c8dff1da3a21f6378607ca391
Jose Carlos Gutierrez <joseco48@gmail.com>
2023-11-02 14:29:10 -0400

    Estructura inicial del proyecto con el primer evento de integracion

---
+++ Restaurant.IntegrationEvents/MenuItemCreado.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.IntegrationEvents;
+
+public class MenuItemCreado
+{
+    public Guid MenuItemId { get; set; }
+    public string Nombre { get; set; }
+    public bool EsReceta { get; set; }
+    public bool EsInventariable { get; set; }
+}
